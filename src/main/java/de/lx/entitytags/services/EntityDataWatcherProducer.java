b455d699ab26b4e3511f9ac747fc92e4e90dae73
Alexander Menze <a.menze@polipol.de>
2020-04-13 14:06:55 +0200

    Interface for dataWatcher util class

---
+++ src/main/java/de/lx/entitytags/services/EntityDataWatcherProducer.java
@@ -0,0 +1,9 @@
+package de.lx.entitytags.services;
+
+import com.comphenix.protocol.wrappers.WrappedDataWatcher;
+
+import org.bukkit.entity.EntityType;
+
+public interface EntityDataWatcherProducer {
+    WrappedDataWatcher Produce(EntityType entityType);
+}
