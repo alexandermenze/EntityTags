4b76bd4b05b720687aa2f3c37dd014c0cccd464b
Alexander Menze <a.menze@polipol.de>
2020-04-18 22:54:46 +0200

    Added handling for all move packets

--- src/main/java/de/lx/entitytags/services/EventInterceptor.java
+++ src/main/java/de/lx/entitytags/services/EventInterceptor.java
@@ -1,30 +1,37 @@
 package de.lx.entitytags.services;
 
+import java.lang.reflect.Field;
+import java.util.Queue;
 import java.util.UUID;
 
-import com.comphenix.packetwrapper.WrapperPlayServerEntityMetadata;
+import com.comphenix.packetwrapper.WrapperPlayServerEntityTeleport;
+import com.comphenix.packetwrapper.WrapperPlayServerRelEntityMove;
+import com.comphenix.packetwrapper.WrapperPlayServerRelEntityMoveLook;
 import com.comphenix.packetwrapper.WrapperPlayServerSpawnEntityLiving;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.events.ListenerPriority;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketEvent;
-import com.comphenix.protocol.wrappers.WrappedDataWatcher;
 
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.plugin.Plugin;
+
+import net.minecraft.server.v1_15_R1.NetworkManager;
 
 public class EventInterceptor extends PacketAdapter implements Listener {
 
     private final EntityTypeService entityTypeService;
     private final EntityIdRepository entityIdRepository;
 
-    private Player player;
-    private int entityId;
+    private Entity entity;
+    private int armorStandEntityId;
 
     public EventInterceptor(Plugin plugin, EntityTypeService entityTypeService, EntityIdRepository entityIdRepository) {
         super(plugin, ListenerPriority.HIGH, PacketType.Play.Server.ENTITY_TELEPORT,
@@ -35,35 +42,61 @@ public class EventInterceptor extends PacketAdapter implements Listener {
 
     @Override
     public void onPacketSending(PacketEvent event) {
+        if(this.entity == null)
+            return;
         
+        Entity entity = getEntityFromPacket(event);
+
+        if (entity == null || entity.getEntityId() != this.entity.getEntityId())
+            return;
+
+        WrapperPlayServerEntityTeleport updateArmorStandWrapper = new WrapperPlayServerEntityTeleport();
+        updateArmorStandWrapper.setEntityID(this.armorStandEntityId);
+        updateArmorStandWrapper.setX(entity.getLocation().getX());
+        updateArmorStandWrapper.setY(entity.getLocation().getY());
+        updateArmorStandWrapper.setZ(entity.getLocation().getZ());
+        updateArmorStandWrapper.setYaw(entity.getLocation().getYaw());
+        updateArmorStandWrapper.setPitch(entity.getLocation().getPitch());
+
+        updateArmorStandWrapper.sendPacket(event.getPlayer());
     }
 
     @EventHandler
-    private void onPlayerJoin(PlayerJoinEvent event){
-        this.player = event.getPlayer();
-        spawnArmorStand(player);
+    private void onPlayerJoin(PlayerInteractEntityEvent event) {
+        this.entity = event.getRightClicked();
+        spawnArmorStand(event.getRightClicked(), event.getPlayer());
     }
 
-    private void updateArmorStand(){
-
-        
+    private void updateArmorStand() {
 
     }
 
-    private void spawnArmorStand(Player player){
-        Location spawnLocation = player.getLocation();
+    private void spawnArmorStand(Entity entity, Player p) {
+        Location spawnLocation = entity.getLocation();
 
-        this.entityId = entityIdRepository.reserve();
+        this.armorStandEntityId = entityIdRepository.reserve();
 
         WrapperPlayServerSpawnEntityLiving packetWrapper = new WrapperPlayServerSpawnEntityLiving();
-        packetWrapper.setEntityID(entityId);
+        packetWrapper.setEntityID(this.armorStandEntityId);
         packetWrapper.setType(this.entityTypeService.getEntityTypeId(EntityType.ARMOR_STAND));
         packetWrapper.setUniqueId(UUID.randomUUID());
         packetWrapper.setX(spawnLocation.getX());
         packetWrapper.setY(spawnLocation.getY());
         packetWrapper.setZ(spawnLocation.getZ());
 
-        packetWrapper.sendPacket(player);
+        packetWrapper.sendPacket(p);
+    }
+
+    private Entity getEntityFromPacket(PacketEvent event) {
+        if (event.getPacketType() == PacketType.Play.Server.REL_ENTITY_MOVE) {
+            return new WrapperPlayServerRelEntityMove(event.getPacket()).getEntity(event);
+        } else if (event.getPacketType() == PacketType.Play.Server.REL_ENTITY_MOVE_LOOK) {
+            return new WrapperPlayServerRelEntityMoveLook(event.getPacket()).getEntity(event);
+        } else if (event.getPacketType() == PacketType.Play.Server.ENTITY_TELEPORT) {
+            return new WrapperPlayServerEntityTeleport(event.getPacket()).getEntity(event);
+        } else {
+            return null;
+        }
     }
 
 }
